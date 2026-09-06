4d7a58a2ce5a906776be453a8b4d74de0618725a
brendonjkding <brendonjkdingnew@gmail.com>
2026-09-06 11:56:39 +0800

    [common] Replace md5sum with md5

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -23,7 +23,7 @@ ifeq ($(THEOS_USE_SEPARATE_LOCAL_DATA_DIR), 1)
 	ifeq ($(_THEOS_REAL_PROJECT_DIR),)
 		_THEOS_REAL_PROJECT_DIR = $(THEOS_PROJECT_DIR)
 	endif
-	_THEOS_LOCAL_DATA_DIR := $(HOME)/.theos/$(shell basename "$(_THEOS_REAL_PROJECT_DIR)").$(shell echo $(_THEOS_REAL_PROJECT_DIR)|md5sum|awk '{print $$1}')
+	_THEOS_LOCAL_DATA_DIR := $(HOME)/.theos/$(shell basename "$(_THEOS_REAL_PROJECT_DIR)").$(shell echo $(_THEOS_REAL_PROJECT_DIR)|md5|awk '{print $$1}')
 endif
 ifneq ($(words $(THEOS_PROJECT_DIR)),1)
 $(shell rm -f /tmp/theos)
